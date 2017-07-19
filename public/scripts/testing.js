c3c1476aa03997fddadc7e53a9bb6c76aba4b98d
Filipescu <andrei.filipescu@sap.com>
2017-07-19 17:35:16 +0300

    more & more

--- public/scripts/testing.js
+++ public/scripts/testing.js
@@ -8,19 +8,23 @@ function calculate(iNumber){
 }
 
 function sign(iSign){
-	parseInt(iCurrentNumber);
+	
 	//aNumbers.push(iCurrentNumber);
 
 	switch(iSign) {
 		case '+': 
+		parseInt(iCurrentNumber);
 		aNumbers.push(iCurrentNumber);
 		aNumbers.push(iSign);
-
+		iResult = iResult + parseInt(iCurrentNumber);
 		break;
 
 		case '-': 
+		parseInt(iCurrentNumber);
 		aNumbers.push(iCurrentNumber);
 		aNumbers.push(iSign); 
+		if(iResult !=0) {iResult = iResult - parseInt(iCurrentNumber);}
+		
 		break;
 
 		case '*': 
@@ -32,19 +36,23 @@ function sign(iSign){
 		break;
 		
 		case '=': 
+		// iResult=0;
+		// aNumbers.push(iCurrentNumber);
+		// for (var i = 0; i < aNumbers.length; i++) {
+		// 	if(aNumbers[i] == '+') {
+		// 		iResult = iResult + parseInt(aNumbers[i-1]) + parseInt(aNumbers[i+1]);
+		// 	}
+		// 	console.log(aNumbers[i]);
+		// 	console.log(iResult);
 		aNumbers.push(iCurrentNumber);
-		for (var i = 0; i < aNumbers.length; i++) {
-			if(aNumbers[i] == '+') {
-				iResult = parseInt(aNumbers[i-1]) + parseInt(aNumbers[i+1]);
-			}
-			console.log(aNumbers[i]);
-			console.log(iResult);
-			document.getElementById('resultInput').value = iResult;
-		}
+		iResult = iResult + parseInt(iCurrentNumber);
+		document.getElementById('calculationInput').value = aNumbers.join(" ");
+		document.getElementById('resultInput').value = iResult;
+		// }
 		break;
 	}
-	
-	document.getElementById('calculationInput').value = aNumbers;
+	document.getElementById('resultInput').value = iResult;
+	document.getElementById('calculationInput').value = aNumbers.join(" ");
 	iCurrentNumber = '';
 	console.log("iCurrentNumber=" + iCurrentNumber);
 	console.log("aNumbers=" + aNumbers);
@@ -52,9 +60,10 @@ function sign(iSign){
 
 function clearResult(){
 	aNumbers = [];
-	iCurrentNumber = '';
-	document.getElementById('calculationInput').value = "";
-	document.getElementById('resultInput').value = "";
+	iCurrentNumber = 0;
+	iResult =0
+	document.getElementById('calculationInput').value = 0;
+	document.getElementById('resultInput').value = 0;
 	console.log("iCurrentNumber=" + iCurrentNumber);
 	console.log("aNumbers=" + aNumbers);
 }
